5e7c1a1788aced40a5f785dfe678a4fd79380270
Will Chen <willchen90@gmail.com>
2025-06-13 15:55:12 -0700

    add component tagger

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+import { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    if (process.env.NODE_ENV === 'development') {
+      config.module.rules.push({
+        test: /\.(jsx|tsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: '@dyad-sh/nextjs-webpack-component-tagger',
+      })
+    }
+    return config
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  // Your Next.js config here
+}
+
+export default withPayload(nextConfig, { devBundleServerPackages: false })
