7223076f99e83fae892a491cb69e41a9ac020d00
Will Chen <willchen90@gmail.com>
2025-07-28 16:05:45 -0700

    Remove migrations (#6)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20250613_215247 from './20250613_215247';
-
-export const migrations = [
-  {
-    up: migration_20250613_215247.up,
-    down: migration_20250613_215247.down,
-    name: '20250613_215247'
-  },
-];
+export const migrations = []
