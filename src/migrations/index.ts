91d5ff76568a6e70741849623a67f74b976a810b
Rahmatullah Zisan <rahmatullahzisan01@gmail.com>
2025-09-08 11:51:57 +0600

    migration data

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,1 +1,9 @@
-export const migrations = []
+import * as migration_20250908_055127 from './20250908_055127';
+
+export const migrations = [
+  {
+    up: migration_20250908_055127.up,
+    down: migration_20250908_055127.down,
+    name: '20250908_055127'
+  },
+];
