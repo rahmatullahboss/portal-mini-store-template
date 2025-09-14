cdb5d4fa18d203ec4524eb314ec1917bab856eaa
Rahmatullah Zisan <rahmatullahzisan01@gmail.com>
2025-09-14 08:05:22 +0600

    Revert "status"
    This reverts commit 8da0e1dc462dd2cfd983f2bdcbc12434c7cbd6ee.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,18 +1,17 @@
-import * as migration_20250908_055127 from './20250908_055127'
-import * as migration_20250909_050442 from './20250909_050442'
-import * as migration_20250909_120000_add_customer_number_to_orders from './20250909_120000_add_customer_number_to_orders'
-import * as migration_20250909_123000_guest_checkout_orders_fields from './20250909_123000_guest_checkout_orders_fields'
-import * as migration_20250909_130000_add_customer_number_to_users from './20250909_130000_add_customer_number_to_users'
-import * as migration_20250910_000001_make_media_alt_nullable from './20250910_000001_make_media_alt_nullable'
-import * as migration_20250911_063423 from './20250911_063423'
-import * as migration_20250911_add_reviews_table from './20250911_add_reviews_table'
-import * as migration_20250911_add_categories_lock_rel from './20250911_add_categories_lock_rel'
-import * as migration_20250912_add_reviews_lock_rel from './20250912_add_reviews_lock_rel'
-import * as migration_20250912_add_reviewer_name_to_reviews from './20250912_add_reviewer_name_to_reviews'
-import * as migration_20250912_make_orders_items_item_nullable from './20250912_make_orders_items_item_nullable'
-import * as migration_20250912_add_device_fields_to_orders from './20250912_add_device_fields_to_orders'
-import * as migration_20250913_add_abandoned_carts from './20250913_add_abandoned_carts'
-import * as migration_20250914_000001_update_order_status_enum from './20250914_000001_update_order_status_enum'
+import * as migration_20250908_055127 from './20250908_055127';
+import * as migration_20250909_050442 from './20250909_050442';
+import * as migration_20250909_120000_add_customer_number_to_orders from './20250909_120000_add_customer_number_to_orders';
+import * as migration_20250909_123000_guest_checkout_orders_fields from './20250909_123000_guest_checkout_orders_fields';
+import * as migration_20250909_130000_add_customer_number_to_users from './20250909_130000_add_customer_number_to_users';
+import * as migration_20250910_000001_make_media_alt_nullable from './20250910_000001_make_media_alt_nullable';
+import * as migration_20250911_063423 from './20250911_063423';
+import * as migration_20250911_add_reviews_table from './20250911_add_reviews_table';
+import * as migration_20250911_add_categories_lock_rel from './20250911_add_categories_lock_rel';
+import * as migration_20250912_add_reviews_lock_rel from './20250912_add_reviews_lock_rel';
+import * as migration_20250912_add_reviewer_name_to_reviews from './20250912_add_reviewer_name_to_reviews';
+import * as migration_20250912_make_orders_items_item_nullable from './20250912_make_orders_items_item_nullable';
+import * as migration_20250912_add_device_fields_to_orders from './20250912_add_device_fields_to_orders';
+import * as migration_20250913_add_abandoned_carts from './20250913_add_abandoned_carts';
 
 export const migrations = [
   {
@@ -48,7 +47,7 @@ export const migrations = [
   {
     up: migration_20250911_063423.up,
     down: migration_20250911_063423.down,
-    name: '20250911_063423',
+    name: '20250911_063423'
   },
   {
     up: migration_20250911_add_reviews_table.up,
@@ -85,9 +84,4 @@ export const migrations = [
     down: migration_20250913_add_abandoned_carts.down,
     name: '20250913_add_abandoned_carts',
   },
-  {
-    up: migration_20250914_000001_update_order_status_enum.up,
-    down: migration_20250914_000001_update_order_status_enum.down,
-    name: '20250914_000001_update_order_status_enum',
-  },
-]
+];
