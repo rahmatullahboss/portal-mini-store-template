26073c5d13a180cc3104083e9aee82aef14e5283
rahmatullahboss <rahmatullahzisan01@gmail.com>
2025-09-15 05:56:10 +0600

    feat: add Google tag

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -36,6 +36,21 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q3LY08VXYN"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-Q3LY08VXYN');
+            `,
+          }}
+        />
+      </head>
       <body>
         <CartProvider>
           <main>{children}</main>
