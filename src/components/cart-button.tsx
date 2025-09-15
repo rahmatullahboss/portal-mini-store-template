948f31b833ba4ac2a282d87660a95d5065321d79
rahmatullahboss <rahmatullahzisan01@gmail.com>
2025-09-15 14:18:31 +0600

    Increase mobile order and cart button heights

--- src/components/cart-button.tsx
+++ src/components/cart-button.tsx
@@ -11,8 +11,8 @@ export const CartButton: React.FC = () => {
   const totalItems = getTotalItems()
 
   return (
-    <Button variant="outline" size="sm" onClick={toggleCart} className="relative">
-      <ShoppingCart className="h-4 w-4" />
+    <Button variant="outline" size="sm" onClick={toggleCart} className="relative h-10 sm:h-8">
+      <ShoppingCart className="h-5 w-5 sm:h-4 sm:w-4" />
       {totalItems > 0 && (
         <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
           {totalItems}
