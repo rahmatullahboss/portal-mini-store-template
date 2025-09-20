37f8237b0c7d35cd18b2a07e28ac25461c3e94b6
Rahmatullah Zisan <rahmatullahzisan01@gmail.com>
2025-09-20 22:04:41 +0600

    Update splash-cursor.tsx

--- src/components/ui/splash-cursor.tsx
+++ src/components/ui/splash-cursor.tsx
@@ -37,7 +37,7 @@ function SplashCursor({
       this.color = [0, 0, 0]
     }
 
-    let config = {
+    const config = {
       SIM_RESOLUTION,
       DYE_RESOLUTION,
       CAPTURE_RESOLUTION,
@@ -55,7 +55,7 @@ function SplashCursor({
       TRANSPARENT,
     }
 
-    let pointers = [new pointerPrototype()]
+    const pointers = [new pointerPrototype()]
 
     const { gl, ext } = getWebGLContext(canvas)
     if (!ext.supportLinearFiltering) {
